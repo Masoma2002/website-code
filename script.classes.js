0e3c2ee420ca654f838c304ffbaa5979a5b2ba60
Masoma2002 <masoma.haidari7@outlook.com>
2023-06-26 10:39:46 +1000

    Removed old website and fixed image structure

--- script.classes.js
+++ script.classes.js
@@ -1,22 +1,25 @@
-let previewclass = document.querySelector('.class-preview');
-let previewBox =  document.querySelector('.preview');
-document.querySelectorAll('.class-container .class-1').forEach(product=>{
-    product.onclick = () =>{
-        previewclass.getElementsByClassName.display = 'flex';
-        let name = product.getAttribute('data-name');
-        previewBox.forEach(preview =>{
-            let target = product.getAttribute('data-target');
-        });
-    };
+let preveiwContainer = document.querySelector('.class-preview');
+let previewBox = Array.from(preveiwContainer.querySelectorAll('.preview'));
+
+document.querySelectorAll('.class-container .class-1').forEach(product => {
+  product.onclick = () => {
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview => {
+      let target = preview.getAttribute('data-target');
+      if (name == target) {
+        preview.classList.add('active');
+      }
+    });
+  };
 });
-previewBox.forEach(close =>{
-    close.onclick = () =>{
-        previewclass.getElementsByClassName.display = 'none';
-        };
-})
-Collapse
 
-
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
 
 
 
